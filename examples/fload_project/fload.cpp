40d9395f2813db45588346409ba31acb755d025f
wa1tnr <wa1tnr@users.noreply.github.com>
2019-08-09 22:30:46 +0000

    polish the presentation a bit

--- examples/fload_project/fload.cpp
+++ examples/fload_project/fload.cpp
@@ -9,6 +9,8 @@
 #include <SdFat.h>
 #include <Adafruit_SPIFlash.h>
 
+#define FILE_NAME      "/forth/ascii_xfer_a001.txt"
+
 #if defined(__SAMD51__) || defined(NRF52840_XXAA)
   Adafruit_FlashTransport_QSPI flashTransport(PIN_QSPI_SCK, PIN_QSPI_CS, PIN_QSPI_IO0, PIN_QSPI_IO1, PIN_QSPI_IO2, PIN_QSPI_IO3);
 #else
@@ -20,14 +22,9 @@
 #endif
 
 Adafruit_SPIFlash flash(&flashTransport);
-
-// file system object from SdFat
 FatFileSystem fatfs;
 
-// Configuration for the file to open and read:
-#define FILE_NAME      "/forth/ascii_xfer_a001.txt"
-
-File thisFile; // external to any function
+File thisFile;
 
 void reading(void) {
   if (thisFile) {
@@ -37,7 +34,7 @@ void reading(void) {
     }
   }
   else {
-    Serial.println("Failed to open data file! Does it exist?");
+    Serial.println("Failed open.");
   }
 }
 
@@ -46,8 +43,8 @@ void fl_setup(void) {
   while (!Serial) {
     delay(100);
   }
-  Serial.println("shred dcmvaa");
-  Serial.println("Adafruit SPI Flash FatFs Simple File Printing Example");
+  Serial.println("\nshred dcm-vbb\n");
+  Serial.println("SPI Flash FatFs file-printout demo");
 
   if (!flash.begin()) {
     Serial.println("Error");
@@ -60,8 +57,11 @@ void fl_setup(void) {
     while(1);
   }
   Serial.println("Mounted filesystem ok");
+  Serial.print("  concat  ");
+  Serial.print(FILE_NAME);
+  Serial.println("   ... to the console:\n");
 
   File dataFile = fatfs.open(FILE_NAME, FILE_READ);
-  thisFile = (File) dataFile; // copy the file handle to a global
+  thisFile = (File) dataFile;
   reading();
 }
